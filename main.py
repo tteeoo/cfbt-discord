9558e5367964e28fb0020ed2571df1d1832253dd
Theo Henson <theodorehenson@protonmail.com>
2025-01-04 01:58:15 -0500

    Work in progress: update meetings, target channel

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python3
 
+import asyncio
 import discord
 from discord.ext import commands
 
 import cor
 
+# TODO: - use tasks 
+# https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html
+# - make cor meetings cog
+# - make events cog
+# https://discordpy.readthedocs.io/en/stable/ext/commands/cogs.html
+
 class BetterTransitBot(commands.Bot):
 
     meeting_cache = []
+    target_channel = ''
 
     async def on_ready(self):
         print(f'Logged on as {self.user}')
+        # Initialize meetings
+        await self.update()
+
+    async def update(self):
+        print('Starting update loop')
+        while True:
+            client.add_meeting(cor.get_meeting())
+            await asyncio.sleep(1)
+
+    @commands.command()
+    async def target(self, ctx):
+        await ctx.send(f'Target channel {ctx.channel}')
 
     def add_meeting(self, m):
+        """Add a meeting to the meeting cache"""
+
+        # Check the meeting is not already added
+        if len(self.meeting_cache) > 0:
+            if m.date == self.meeting_cache[0].date:
+                print(f'Duplicate meeting: {m}')
+                return
+
+        # Add the meeting
         self.meeting_cache.insert(0, m)
         print(f'Added meeting: {m}')
 
@@ -27,7 +56,7 @@ if __name__ == '__main__':
     intents = discord.Intents.default()
     intents.message_content = True
     client = BetterTransitBot(command_prefix='!', intents=intents)
-    #client.run(token)
 
-    # Initialize meetings
-    client.add_meeting(cor.get_meeting())
+    # Run bot
+    client.run(token)
+
