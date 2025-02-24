0ef0ccbca3cccdfa9fbf0122a46a1b7674809f1f
Theo Henson <theodorehenson@protonmail.com>
2025-02-24 00:05:51 -0600

    Implement file for persistant cache

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import ast
 import asyncio
 import discord
 from discord.ext import commands, tasks
@@ -14,36 +15,54 @@ import cor
 
 class BetterTransitBot(commands.Bot):
 
-    meeting_cache = []
-    target_channel = ''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    async def on_ready(self):
-        print(f'Logged on as {self.user}')
+        meeting_dates_cache = set({})
+        target_channel = ''
+
+        # Get meeting dates cache from file
+        try:
+            with open('cfbt-cache.txt', 'r') as f:
+                self.meeting_dates_cache = ast.literal_eval(f.read())
+        except FileNotFoundError:
+            pass
 
         # Get meeting agenda output channel ID
         with open('channel.txt', 'r') as f:
-            target_channel = f.readline().strip()
-        print(f'Sending meeting agendas to channel ID {target_channel}')
+            self.target_channel = f.readline().strip()
+        print(f'Sending meeting agendas to channel ID {self.target_channel}')
+
+    async def on_ready(self):
+        """Runs when the bot connects successfully"""
+
+        print(f'Logged on as {self.user}')
 
         print(f'Starting update task loop')
         self.update.start()
 
     @tasks.loop(seconds=5.0)
     async def update(self):
+        """Event loop for checking for new meetings"""
         self.add_meeting(cor.get_recent())
 
     def add_meeting(self, m):
         """Add a meeting to the meeting cache"""
 
         # Check the meeting is not already added
-        if len(self.meeting_cache) > 0:
-            if m.date == self.meeting_cache[0].date:
-                print(f'Duplicate meeting: {m}')
-                return
+        if m.date not in self.meeting_dates_cache:
 
-        # Add the meeting
-        self.meeting_cache.insert(0, m)
-        print(f'Added meeting: {m}')
+            # Add the meeting
+            self.meeting_dates_cache.add(m.date)
+            print(f'New meeting: {m}')
+
+            # Update the cache
+            with open('cfbt-cache.txt', 'w') as f:
+                f.write(str(self.meeting_dates_cache))
+            print('Updated cache')
+
+        else:
+            print(f'Duplicate meeting: {m}')
 
 if __name__ == '__main__':
 
@@ -54,6 +73,8 @@ if __name__ == '__main__':
 
     # Create and run bot
     intents = discord.Intents.default()
+    # Following line is necessary for newer versions of discord.py,
+    #    but does not work for the version compatible with cs1 
     # intents.message_content = True
     client = BetterTransitBot(command_prefix='!', intents=intents)
 
